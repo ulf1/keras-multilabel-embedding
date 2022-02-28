8ab3339c228649a868d5b8c6472db79de1b94f89
UH <554c46@gmail.com>
2022-02-28 12:43:33 +0100

    fix #2

---
+++ keras_multilabel_embedding/fixednumlabels.py
@@ -0,0 +1,51 @@
+import tensorflow as tf 
+from typing import List, Union
+
+
+def parse_args_initializer(initializer: Union[str, float, list]
+                           ) -> tf.keras.initializers.Initializer:
+    """Check process initializer input arguments"""
+    if isinstance(initializer, str):
+        return tf.keras.initializers.get(initializer)
+    elif isinstance(initializer, (int, float, list)):
+        return tf.keras.initializers.Constant(initializer)
+    elif isinstance(initializer, tf.keras.initializers.Initializer):
+        return initializer
+    else:
+        return None
+
+
+class MultiLabelEmbedding(tf.keras.layers.Layer):
+    def __init__(self,
+                 vocab_size: int = None,
+                 embed_size: int = None,
+                 random_state: int = None,
+                 initializer: Union[str, float, list] = 'ones',
+                 regularizer: str = None,
+                 constraint: str = None,
+                 **kwargs):
+        super(MultiLabelEmbedding, self).__init__(**kwargs)
+        # store hyper params
+        self.vocab_size = vocab_size   # v
+        self.embed_size = embed_size   # e
+        # other settings
+        self.initializer = parse_args_initializer(initializer)
+        self.regularizer = tf.keras.regularizers.get(regularizer)
+        self.constraint = tf.keras.constraints.get(constraint)
+        # initialize layer weights
+        if random_state:
+            tf.keras.utils.set_random_seed(random_state)
+
+    def build(self, input_shape=None):
+        self.emb = self.add_weight(
+            shape=(self.vocab_size, self.embed_size),
+            initializer=self.initializer,
+            regularizer=self.regularizer,
+            constraint=self.constraint,
+            trainable=self.trainable,
+            name='multi_label_embedding')
+
+    def call(self, inputs):
+        h = tf.nn.embedding_lookup(params=self.emb, ids=inputs)
+        h = tf.math.reduce_sum(h, axis=-2)
+        return h
---
+++ test/test_fixednumlabels.py
@@ -0,0 +1,23 @@
+from keras_multilabel_embedding import MultiLabelEmbedding
+import tensorflow as tf
+
+
+def test1():
+    # the lookup based Embedding
+    x_ids = [[1, 2, 4], [0, 1, 2], [2, 1, 4], [3, 2, 1]]
+    x_ids = tf.constant(x_ids)
+    layer1 = MultiLabelEmbedding(
+        vocab_size=5, embed_size=300, random_state=42,
+        initializer=tf.keras.initializers.VarianceScaling(seed=42))
+    layer1.build()
+    y1 = layer1(x_ids)
+    # the linear layer
+    x_ids = [[0., 1, 1, 0, 1], [1, 1, 1, 0, 0],
+                [0, 1, 1, 0, 1], [0, 1, 1, 1, 0]]
+    x_ids = tf.constant(x_ids)
+    layer2 = tf.keras.layers.Dense(300, use_bias=False)
+    layer2.build(input_shape=(None, 5))
+    layer2.set_weights(layer1.get_weights()) 
+    y2 = layer2(x_ids)
+    # compare
+    tf.debugging.assert_near(y1, y2)
